af8a19e202e089c7f67b12e9b364769244e7a312
Bajenov Nikolay <bajenov.nikolay@gmail.com>
2019-04-03 22:05:07 +0600

    ==

--- src/Sender.php
+++ src/Sender.php
@@ -11,14 +11,14 @@ class Sender
     private $url;
     private $method;
 
-    public function __construct(string $sendToUrl, string $method)
+    public function __construct(string $sendToUrl)
     {
         if (!filter_var($sendToUrl, FILTER_VALIDATE_URL)) {
             throw new \Exception('Url is not valid');
         }
 
-        $this->url = $sendToUrl;
-        $this->method = $method;
+        $this->url = parse_url($sendToUrl, PHP_URL_HOST);
+        $this->method = parse_url($sendToUrl, PHP_URL_PATH) . '?' . parse_url($sendToUrl, PHP_URL_QUERY);
     }
 
     public function send(array $data, array $headers = [])
